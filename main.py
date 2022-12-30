1ae7a91b5f36e7e2be8153090ee7f4341237183c
MLTimetoCode <120279175+MLTime2Code@users.noreply.github.com>
2022-12-30 04:37:59 +0000

    changes the code

--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-order = input('Hello! What would you like to order? (Salad, Pasta, Pizza, or Hamburguer ')
-menu_choices = random.choice(['Salad', 'Pasta','Pizza','Hamburger'])
+order = input('Welcome to the Bistro! What would you like to do?')
+bistro_options = {
+  'Check the menu'
+  ''
+}
+def chat(responses):
+  if 
