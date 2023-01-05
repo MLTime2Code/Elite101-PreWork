00fab56523d4c7d686087812e8fd8f23454accb1
MLTimetoCode <120279175+MLTime2Code@users.noreply.github.com>
2023-01-05 03:42:36 +0000

    Updated code and recovered previous README file content

---
+++ README.md
@@ -0,0 +1,10 @@
+# Elite 101-PreWork
+This is the Code2College Elite101 PreWork Chatbox assignment.
+The Chatbox is intended to simulate a conversation by asking questions and replying to responses.
+The Chatbox is made using Python.
+
+# Testing Branch
+This branch will be pushed to the repository as a new branch
+The branch function allows users to create seperate changes without interferring with the main one.
+Once ready, users can merge branches to the main.
+(This is based on from what I can remember)
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-order = input('Welcome to The Bistro! What would you like to do?\n Check the menu or Place an order ')
+order = input('Welcome to The Bistro! What would you like to do?\n Check the menu, Place an order, Make a reservation ')
 
-
+#----------------------------------------------------------
 if "Check the menu" in order:
-  print("Here are the options")
-  
-options = input('Salad, Sandwhich, or Panini ')
+  print("Here are the options" + options)
 
+options = ('Salad, Sandwhich, or Panini ')
 
 if "Salad" in options:
   print("The Salad is a combination of various green vegetables like cabbage and spinach and is sprinkled in cheese. It comes with a side of dressing.")
@@ -15,8 +14,6 @@ if "Sandwhich" in options:
 
 if "Panini" in options:
   print("Our Panini is like a sandwhich but grilled and made with delicious italian bread.")
-
-order = input('Welcome to The Bistro! What would you like to do?\n Check the menu or Place an order: ')
 
 if "Place an order" in order:
   print("Alright, what would you like to order?")
