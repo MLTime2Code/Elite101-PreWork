0b42179b935fbc9484e8ad6af21ae2b92cca6b36
MLTimetoCode <120279175+MLTime2Code@users.noreply.github.com>
2023-01-09 00:41:37 +0000

    changes to the code to become better

--- main.py
+++ main.py
@@ -1,47 +1,52 @@
-order = input('Welcome to The Bistro! What would you like to do?\nCheck the menu, Place an order, Make a reservation ')
+order = input('Welcome to The Bistro! What would you like to do?\n Check the menu, Place an order, Make a reservation ')
 
 #----------------------------------------------------------
-
-if "Check the menu" in order:
-  print("Here are the options")
-
-  options = input('Salad, Sandwhich, or Panini ')
-
-  if "Salad" in options:
-    print("Our Salad is a combination of various green vegetables like cabbage and spinach and is sprinkled in cheese. It comes with a side of dressing.")
+while order == "Check the menu":
+    if "Check the menu" in order:
+      print("Here are the options")
+  
+      options = input('Salad, Sandwhich, or Panini ')
+      another = input('Would you like to do something else? Yes or No ')
+      if "Yes" in another:
+          break
+        
+      if "No" in another:
+        if "Salad" in options:
+         print("Our Salad is a combination of various green vegetables like cabbage and spinach and is sprinkled in cheese. It comes with a side of dressing.")
+      
+        if "Sandwhich" in options:
+          print("Our Sandwhich is made with fresly made bread and contains ham, mayonaise, mustard, cheese, lettuce and a special sauce.")
     
-  if "Sandwhich" in options:
-    print("Our Sandwhich is made with fresly made bread and contains ham, mayonaise, mustard, cheese, lettuce and a special sauce.")
+        if "Panini" in options:
+          print("Our Panini is like a sandwhich but grilled and made with delicious italian bread.")
   
-  if "Panini" in options:
-    print("Our Panini is like a sandwhich but grilled and made with delicious italian bread.")
-  
-#----------------------------------------------------------
-elif "Place an order" in order:
-  print("Alright, what would you like to order?")
-  
-  options = input('Salad, Sandwhich, or Panini ')
-  
-  if "Salad" in options:
-    print("A healthy option, it will be ready quickly.")
+  #----------------------------------------------------------
+while order == "Place an order":
+    if "Place an order" in order:
+      print("Alright, what would you like to order?")
     
-  if "Sandwhich" in options:
-    print("Okay, your order will arrive soon!")
+      options = input('Salad, Sandwhich, or Panini ')
+      another = input('Would you like to do something else? Yes or No ')
+      if "Yes" in another:
+        break
+        
+      if "No" in another:
+        if "Salad" in options:
+          print("A healthy option, it will be ready quickly.")
+      
+        if "Sandwhich" in options:
+          print("Okay, your order will arrive soon!")
+      
+        if "Panini" in options:
+         print("It will take more time to prepare but the wait will be worth it!")
+  
+  #----------------------------------------------------------
+if "Make a reservation" in order:
+      print("At what time and day would you like to reserve?")
+      print("Please write the month, day, and time")
+      month_day = input('What month and day would that be? ')
+      time = input ('At what time? ')
     
-  if "Panini" in options:
-    print("It will take more time to prepare but the wait will be worth it!")
-    
+      print("Fortunately, " + month_day + " at " + time + " will be available. We hope to see you then. " )
 
-#----------------------------------------------------------
-elif "Make a reservation" in order:
-  print("At what time and day would you like to reserve?")
-  print("Please write the month, day, and time")
-  
-  month_day = input('What month and day would that be? ')
-  time = input ('At what time? ')
-  
-  print("Fortunately, " + month_day + " at " + time + " will be available. We hope to see you then. " )
 #------------------------------------------------------------
-while order != "Check the menu"or"Place an order"or"Make a reservation":
-  print("Could you repeat that?")
-  order = input('Welcome to The Bistro! What would you like to do?\n Check the menu, Place an order, Make a reservation ')
